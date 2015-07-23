c0fba947ddf6eda66cfbbf7e0e05b7148b38aa3f
Valeri Karpov <valkar207@gmail.com>
2015-07-23 14:06:56 -0400

    Add basic facebook login

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var PassportConfigurator =
+  require('loopback-component-passport').PassportConfigurator;
 
 var app = module.exports = loopback();
+
+app.use(loopback.session({ secret: 'keyboard cat' }));
 
 app.start = function() {
   // start the web server
@@ -15,6 +19,17 @@ app.start = function() {
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
   if (err) throw err;
+
+  // Create an instance of PassportConfigurator with the app instance
+  var passportConfigurator = new PassportConfigurator(app);
+  passportConfigurator.init();
+  passportConfigurator.setupModels({
+    userModel: app.models.User,
+    userIdentityModel: app.models.UserIdentity,
+    userCredentialModel: app.models.UserCredential
+  });
+  passportConfigurator.configureProvider('facebook-login',
+    require('../providers.json')['facebook-login']);
 
   // start the server if `$ node server.js`
   if (require.main === module)
